9325798350e0cf9fb06bafae62c85c90f5e1c7a8
meleswujira <manzeno@gmail.com>
2014-07-10 02:18:58 -0700

    All the four plots are ploted

---
+++ plot2.R
@@ -0,0 +1,10 @@
+columns<-c("character", "character", "numeric", "numeric", "numeric", "numeric","numeric", "numeric", "numeric")
+hhpc = read.table("household_power_consumption.txt", sep=";", header = TRUE,colClasses = columns, na.strings = "?")
+#head(hhpc)
+hhpc$Date_Time = strptime(paste(hhpc$Date, hhpc$Time),format="%d/%m/%Y %H:%M:%S")
+hhpc <- subset(hhpc,as.Date(Date_Time) >= as.Date("2007-02-01") & as.Date(Date_Time) <= as.Date("2007-02-02"))))
+
+png("plot2.png", height=480, width=480) 
+plot(hhpc$Date_Time, hhpc$Global_active_power, pch=NA, xlab="", ylab="Global Active Power (kilowatts)") 
+lines(hhpc$Date_Time, hhpc$Global_active_power) 
+dev.off() 
---
+++ plot3.R
@@ -0,0 +1,13 @@
+columns<-c("character", "character", "numeric", "numeric", "numeric", "numeric","numeric", "numeric", "numeric")
+hhpc = read.table("household_power_consumption.txt", sep=";", header = TRUE,colClasses = columns, na.strings = "?")
+#head(hhpc)
+hhpc$Date_Time = strptime(paste(hhpc$Date, hhpc$Time),format="%d/%m/%Y %H:%M:%S")
+hhpc <- subset(hhpc,as.Date(Date_Time) >= as.Date("2007-02-01") & as.Date(Date_Time) <= as.Date("2007-02-02"))
+
+png("plot3.png", height=480, width=480)
+plot(hhpc$Date_Time, hhpc$Sub_metering_1, pch=NA, xlab="", ylab="Energy sub metering") 
+lines(hhpc$Date_Time, hhpc$Sub_metering_1) 
+lines(hhpc$Date_Time, hhpc$Sub_metering_2, col='red') 
+lines(hhpc$Date_Time, hhpc$Sub_metering_3, col='blue') 
+legend('topright', c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty = c(1,1,1), col = c('black', 'red', 'blue'))
+dev.off() 
